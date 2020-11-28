ee1b05d49de3a41768e164df7ff1a7b4a99c6b34
userajent <adrian.gearing@gmail.com>
2020-11-28 14:33:27 +1000

    draft data access object

--- data/dao.js
+++ data/dao.js
@@ -1,6 +1,19 @@
 // To Generate Models:
 // node_modules/.bin/sequelize-auto -o "./data/models" -d myrv.db -h localhost -e sqlite
 
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DBPATH)
+process.env.DBPATH = "myrv.db";
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DBPATH);
+const models = require('data/models/init-models')(sequelize);
+const dao = () => {
+    var getOwner: async (id) => {
+        data = await models.owner.findAll({where: {id: id}})
+        return data;
+    }
+}
+
+
+module.exports = dao;
+module.exports.dao = dao;
+module.exports.default = dao;

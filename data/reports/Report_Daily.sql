5b41ff5fdc607c4787377244f21fce42951ea149
userajent <adrian.gearing@gmail.com>
2021-08-07 06:56:19 +1000

    Updated amonthly reports to a table dump and daily reports to month-to-date changes

--- data/reports/Report_Daily.sql
+++ data/reports/Report_Daily.sql
@@ -2,31 +2,31 @@
 .mode csv
 .headers on
 
--- Report of all vehicle recall items updated in last 7 days
+-- Report of all vehicle recall items updated month to date
 .output data/reports/temp/recallReport.csv
 SELECT v.id, 'v.ipa' AS `IPA`, v.buildNo, v.vin,
   rI.description, vRI.status, vRI.updatedBy, vRI.updatedAt, vRI.id
 FROM vehicle AS `v` 
   INNER JOIN vehicleRecallItem AS `vRI` ON v.id = vRI.vehicleId
   INNER JOIN recallItem AS `rI` ON vrI.recallItemId = rI.id
-WHERE vrI.updatedAt BETWEEN date('now', '-7 days') AND datetime('now');
+WHERE vrI.updatedAt BETWEEN date('now', 'start of month') AND datetime('now');
 
--- Report of all owner details updated in the last 7 days
+-- Report of all owner details updated month to date
 .output data/reports/temp/ownerReport.csv
 SELECT v.buildNo, v.vin, o.name, o.email, o.phone, o.street, o.suburb, o.state, o.postcode, o.updatedBy, o.updatedAt
 FROM owner AS `o`
   INNER JOIN vehicle AS `v` ON o.vehicleId = v.id
-WHERE o.updatedAt BETWEEN date('now', '-7 days') AND datetime('now');
+WHERE o.updatedAt BETWEEN date('now', 'start of month') AND datetime('now');
 
--- Report of all contact records updated in the last 7 days
+-- Report of all contact records updated month to date
 .output data/reports/temp/contactReport.csv
 SELECT v.vin, v.buildNo, rC.action, rC.response, rF.name, rF.tag, rF.feedback, rC.updatedBy, rC.updatedAt
 FROM recallContact as `rC`
   INNER JOIN vehicle AS `v` ON rC.vehicleId = v.id
   LEFT JOIN recallFeedback AS `rF` ON rC.id = rF.recallContactId
-WHERE rC.updatedAt BETWEEN date('now', '-7 days') AND datetime('now');
+WHERE rC.updatedAt BETWEEN date('now', 'start of month') AND datetime('now');
 
--- NTI Report - all Owner or Vehicle records updated in the last 7 days
+-- NTI Report - all Owner or Vehicle records updated month to date
 .output data/reports/temp/NtiReport.csv
 SELECT v.id, v.ipa, v.buildNo, v.vin, v.engineNo, v.modelDesc, v.addSpec, v.variantCode, o.id, o.vehicleId, o.name, o.email, o.phone, o.street, o.suburb, o.state, o.postcode, o.regoState, o.regoNo, o.regoDt, v.updatedBy AS vehicleUpdatedBy, v.updatedAt AS vehicleUpdatedAt, o.updatedBy AS ownerUpdatedBy, o.updatedAt AS ownerUpdatedAt
 FROM vehicle AS `v` 
@@ -38,5 +38,5 @@ FROM vehicle AS `v`
     ORDER BY id
   ) AS `o` ON v.id = o.vehicleId
   WHERE 
-    v.updatedAt BETWEEN date('now', '-7 days') AND datetime('now')
-    OR o.updatedAt BETWEEN date('now', '-7 days') AND datetime('now');
+    v.updatedAt BETWEEN date('now', 'start of month') AND datetime('now')
+    OR o.updatedAt BETWEEN date('now', 'start of month') AND datetime('now');
--- data/reports/Report_Monthly.sql
+++ data/reports/Report_Monthly.sql
@@ -2,73 +2,26 @@
 .mode csv
 .headers on
 
--- Report of all vehicle recall items updated this month
-.output data/reports/temp/recallUpdates_Monthly.csv
-SELECT v.id, v.ipa, v.buildNo, v.vin,
-  rI.description, vRI.status, vRI.updatedBy, vRI.updatedAt, vRI.id
-FROM vehicle AS `v` 
-  INNER JOIN vehicleRecallItem AS `vRI` ON v.id = vRI.vehicleId
-  INNER JOIN recallItem AS `rI` ON vrI.recallItemId = rI.id
-WHERE vrI.updatedAt BETWEEN date('now', 'start of month', '-1 months') AND date('now', 'start of month');
+.mode csv
+.headers on
 
--- Report of all owner details updated this month
-.output data/reports/temp/ownerUpdates_Monthly.csv
-SELECT v.buildNo, v.vin, o.name, o.email, o.phone, o.street, o.suburb, o.state, o.postcode, o.updatedBy, o.updatedAt
-FROM owner AS `o`
-  INNER JOIN vehicle AS `v` ON o.vehicleId = v.id
-WHERE o.updatedAt BETWEEN date('now', 'start of month', '-1 months') AND date('now', 'start of month');
+.output data/reports/temp/accessLog.csv
+SELECT * FROM accessLog;
 
--- Report of all contact records updated this month
-.output data/reports/temp/contactUpdates_Monthly.csv
-SELECT v.vin, v.buildNo, rC.action, rC.response, rF.name, rF.tag, rF.feedback, rC.updatedBy, rC.updatedAt
-FROM recallContact as `rC`
-  INNER JOIN vehicle AS `v` ON rC.vehicleId = v.id
-  LEFT JOIN recallFeedback AS `rF` ON rC.id = rF.recallContactId
-WHERE rC.updatedAt BETWEEN date('now', 'start of month', '-1 months') AND date('now', 'start of month');
+.output data/reports/temp/recallContact.csv
+SELECT * FROM recallContact;
 
--- Recall Items Status Report
-.output data/reports/temp/MonthlyReporting_RecallStatus.csv
-SELECT v.id, v.ipa, v.buildNo, v.vin,
-  rI.description, vRI.status, vRI.updatedBy, vRI.updatedAt, vRI.id
-FROM vehicle AS `v` 
-  INNER JOIN vehicleRecallItem AS `vRI` ON v.id = vRI.vehicleId
-  INNER JOIN recallItem AS `rI` ON vrI.recallItemId = rI.id;
+.output data/reports/temp/recallItem.csv
+SELECT * FROM recallItem;
 
--- Recall Owner Contact Status Report
-.output data/reports/temp/MonthlyReporting_OwnerStatus.csv
-SELECT v.vin, v.buildNo, q_O.name AS `owner`, q_rC.action, q_rC.response, q_rC.updatedBy, q_rC.updatedAt
-FROM vehicle as `v`
-  INNER JOIN (SELECT vehicleId FROM vehicleRecallItem GROUP BY vehicleId) AS `q_vRI` ON v.id = q_vRI.vehicleId
-  LEFT JOIN (SELECT vehicleId, name FROM owner GROUP BY vehicleId HAVING id = MAX(id)) AS `q_O` ON v.id = q_O.vehicleId
-  LEFT JOIN (SELECT vehicleId, response, action, updatedBy, updatedAt FROM recallContact WHERE response IS NOT NULL GROUP BY vehicleId HAVING id = MAX(id)) AS `q_rC` ON v.id = q_rC.vehicleId;
+.output data/reports/temp/vehicleRecallItem.csv
+SELECT * FROM vehicleRecallItem;
 
--- Current Owners Report
--- Gets most recent owner records by vehicleId then filters for active records only
-.output data/reports/temp/MonthlyReporting_CurrentOwners.csv
-SELECT v.buildNo, v.vin, o.id AS `ownerId`, o.vehicleId, o.name, o.email, o.phone, o.street, o.suburb, o.state, o.postcode, o.regoState, o.regoNo, o.regoDt, o.createdAt AS ownerCreatedAt, o.createdBy AS ownerCreatedBy, o.updatedBy AS ownerUpdatedBy, o.updatedAt AS ownerUpdatedAt
-FROM vehicle AS `v` 
-  LEFT JOIN (
-    SELECT *
-    FROM (
-      SELECT id, vehicleId, name, email, phone, street, suburb, state, postcode, regoState, regoNo, regoDt, isActive, createdBy, updatedBy, createdAt, updatedAt 
-      FROM owner
-      GROUP BY vehicleId 
-      HAVING ROWID = MAX(ROWID) 
-      ORDER BY id
-    ) WHERE isActive = 1
-  ) AS `o` ON v.id = o.vehicleId;
+.output data/reports/temp/owner.csv
+SELECT * FROM owner;
 
--- NTI Report - all Owner or Vehicle records updated in the last month
-.output data/reports/temp/NtiReport.csv
-SELECT v.id, v.ipa, v.buildNo, v.vin, v.engineNo, v.modelDesc, v.addSpec, v.variantCode, o.id, o.vehicleId, o.name, o.email, o.phone, o.street, o.suburb, o.state, o.postcode, o.regoState, o.regoNo, o.regoDt, v.updatedBy AS vehicleUpdatedBy, v.updatedAt AS vehicleUpdatedAt, o.updatedBy AS ownerUpdatedBy, o.updatedAt AS ownerUpdatedAt
-FROM vehicle AS `v` 
-  LEFT JOIN (
-    SELECT id, vehicleId, name, email, phone, street, suburb, state, postcode, regoState, regoNo, regoDt, createdBy, updatedBy, createdAt, updatedAt 
-    FROM owner
-    GROUP BY vehicleId 
-    HAVING ROWID = MAX(ROWID) 
-    ORDER BY id
-  ) AS `o` ON v.id = o.vehicleId
-  WHERE 
-    v.updatedAt BETWEEN date('now', 'start of month', '-1 months') AND date('now', 'start of month')
-    OR o.updatedAt BETWEEN date('now', 'start of month', '-1 months') AND date('now', 'start of month');
+.output data/reports/temp/recallFeedback.csv;
+SELECT * FROM recallFeedback;
+
+.output data/reports/temp/vehicle.csv
+SELECT * FROM vehicle;

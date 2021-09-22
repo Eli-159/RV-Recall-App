fe8468c1adeff021a95455d4f5191993f87688c9
Eli <64872006+Eli-159@users.noreply.github.com>
2021-09-23 08:51:13 +1000

    Authentication Pages Created

--- routes/google.js
+++ routes/google.js
@@ -14,10 +14,38 @@ router.get('/auth/code', (req, res, next) => {
     const givenScopes = JSON.stringify(req.query.scope.includes(' ') ? req.query.scope.split(' ').sort() : req.query.scope);
     const givenCode = req.query.code;
     if (requestedScopes == givenScopes) {
-        gmail.createTokenFromCode(givenCode).then(() => gmail.sendEmail().then(() => res.redirect('/workshop/actions'))).catch(err => console.log(err));
+        gmail.createTokenFromCode(givenCode).then(() => {
+            res.redirect('/workshop/admin/google/auth/success');
+        }).catch(err => {
+            res.redirect('/workshop/admin/google/auth/error');
+        });
     } else {
-        res.redirect('/workshop/admin/google/auth/url');
+        res.redirect('/workshop/admin/google/auth/scope-error');
     }
+});
+
+router.get('/auth/success', (req, res, next) => {
+    res.render('workshop/admin/google/auth-success', {
+        pageTitle: 'Authentication Success',
+        path: '/workshop/admin/google/auth/success',
+        role: req.payload.role
+    });
+});
+
+router.get('/auth/error', (req, res, next) => {
+    res.render('workshop/admin/google/auth-error', {
+        pageTitle: 'Authentication Error',
+        path: '/workshop/admin/google/auth/error',
+        role: req.payload.role
+    });
+});
+
+router.get('/auth/scope-error', (req, res, next) => {
+    res.render('workshop/admin/google/scope-error', {
+        pageTitle: 'Authentication Success',
+        path: '/workshop/admin/google/auth/scope-error',
+        role: req.payload.role
+    });
 });
 
 // The router is exported.
